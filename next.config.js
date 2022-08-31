cad73e569819cb44731bf129b684d394e3c42aac
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-08-31 09:41:19 +0700

    AnhLPT/config-env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  env: {
+      "BASE_URL": "http://localhost:3000",
+      "MONGODB_URL": "0.0.0.0/0",
+      "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
+      "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
+      "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
+      "CLOUD_UPDATE_PRESET": "YOUR_CLOUD_UPDATE_PRESET",
+      "CLOUD_NAME": "YOUR_CLOUD_NAME",
+      "CLOUD_API": "YOUR_CLOUD_API"
+  }
 }
-
-module.exports = nextConfig
