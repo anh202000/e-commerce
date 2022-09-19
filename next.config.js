24c108686b3c98a144648e5e5d0dc634f49b87d2
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-09-19 23:24:10 +0700

    feature/anhlpt/config env [cloud uload img, paypal id, token secret]

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 module.exports = {
   env: {
       "BASE_URL": "http://localhost:3000",
-      "MONGODB_URL": "mongodb+srv://anh:anh@cluster0.lsbrsab.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
-      "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
-      "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
-      "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
-      "CLOUD_UPDATE_PRESET": "YOUR_CLOUD_UPDATE_PRESET",
-      "CLOUD_NAME": "YOUR_CLOUD_NAME",
-      "CLOUD_API": "YOUR_CLOUD_API"
+      // connect cloud mogodb
+      "MONGODB_URL": "mongodb+srv://anh:anh@cluster0.pzq4wws.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
+      // https://www.passwordsgenerators.net/ config access token với Password Length: 50 là char
+      "ACCESS_TOKEN_SECRET": "hCma3RhMFbMgJ9FUN4]z$q`-}b?Mq>Ed2KU#F'(QR.w^*5q`<D",
+      // https://www.passwordsgenerators.net/ config access token với Password Length: 80 là char
+      "REFRESH_TOKEN_SECRET": "c4BNF[({`cP;g6%&9>.&::[!mA(V2Z6_dtgDss']u%Cj~M?#_,/}/A7%J8FJzsm&9k9Z!';k*~*^wJ",
+      // link app test paypal https://developer.paypal.com/dashboard/applications/edit/SB:QVpnVy0yalhWOGFCdEZRMUpnX05jRVo0QVhDMDRhdUFjcE5FOWVoUDBaQ2xKenZuRTVfekhLbjhiTVFFUDVpM2Y4TFZMSWFFVlhPQlBoTFI=
+      // account payment anhroyal110 Anh202000 / 0962731018
+      "PAYPAL_CLIENT_ID": "AZgW-2jXV8aBtFQ1Jg_NcEZ4AXC04auAcpNE9ehP0ZClJzvnE5_zHKn8bMQEP5i3f8LVLIaEVXOBPhLR",
+      "CLOUD_UPDATE_PRESET": "cloudfilesAnhLPT",
+      "CLOUD_NAME": "anhlptstore",
+      "CLOUD_API": "https://api.cloudinary.com/v1_1/anhlptstore/image/upload/"
   }
 }
