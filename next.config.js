5757aea8b840052d8ecc14cbd7004cfccf68cfdc
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-09-19 22:08:08 +0700

    feature/update env with mongo

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   env: {
       "BASE_URL": "http://localhost:3000",
-      // "MONGODB_URL": "mongodb+srv://anh:anh@cluster0.lsbrsab.mongodb.net/test",
-      "MONGODB_URL": "mongodb+srv://devat:devat123@cluster0.kzbsp.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
+      "MONGODB_URL": "mongodb+srv://anh:anh@cluster0.lsbrsab.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
       "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
       "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
       "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
