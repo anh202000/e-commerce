5ffabc87e2cd023b937e83feac1ae0f3462b7819
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-09-19 18:26:37 +0700

    feature/create-connect-mongodb add packgage

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
       "BASE_URL": "http://localhost:3000",
-      "MONGODB_URL": "0.0.0.0/0",
+      // "MONGODB_URL": "mongodb+srv://anh:anh@cluster0.lsbrsab.mongodb.net/test",
+      "MONGODB_URL": "mongodb+srv://devat:devat123@cluster0.kzbsp.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
       "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
       "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
       "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
