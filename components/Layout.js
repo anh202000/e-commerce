5fb90739608dcb13ad6582afa442f1b928f3e89f
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-11-15 01:23:38 +0700

    feature messenger

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,14 @@ import React from "react";
 import NavBar from "./NavBar";
 import Notify from "./Notify";
 import Modal from "./Modal";
+import Mess from "../public/mess.js"
 
 function Layout({ children }) {
+
+    const newPopup = (url) => {
+        window.open(
+            url, 'popUpWindow', 'height=600,width=400,right=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+    }
     return (
         <div>
             <div className="container">
@@ -11,12 +17,16 @@ function Layout({ children }) {
             </div>
 
             <p class="line-hight"></p>
-            
+
             <div className="container">
                 <Notify />
                 <Modal />
+                <div onClick={() => newPopup('https://nextjsblogs.herokuapp.com/')} style={{ position: 'fixed', bottom: '2%', right: '2%', zIndex: 999 }}>
+                    <Mess />
+                </div>
                 {children}
             </div>
+
         </div>
     );
 }
