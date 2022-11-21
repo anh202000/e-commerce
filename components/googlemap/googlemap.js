539244a9058abc365f103c0a7d79a45bc291b0c0
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-11-21 19:21:34 +0700

    fixbug

--- components/googlemap/googlemap.js
+++ components/googlemap/googlemap.js
@@ -9,7 +9,7 @@ function GoogleApiWrapper() {
         frameborder="0"
         scrolling="no"
         marginheight="0"
-        marginwidth="0"
+        marginWidth="0"
         id="gmap_canvas"
         src="https://maps.google.com/maps?width=887&amp;height=583&amp;hl=en&amp;q=910A%20Ng%C3%B4%20Quy%E1%BB%81n,%20An%20H%E1%BA%A3i%20B%E1%BA%AFc,%20S%C6%A1n%20Tr%C3%A0,%20%C4%90%C3%A0%20N%E1%BA%B5ng%20550000%20Tuy%20Hoa+(Youngz%20Shop)&amp;t=p&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
       ></iframe>
